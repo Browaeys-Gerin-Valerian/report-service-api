ec64b6e70997894d0c8c96db2597f7efd6488abb
Valerian <valgerin@hotmail.fr>
2025-11-22 13:35:05 +0100

    fix: add description field in blueprint and blueprint ref in template

--- src/models/blueprint.model.ts
+++ src/models/blueprint.model.ts
@@ -5,6 +5,7 @@ import { IBlueprint } from "../interfaces";
 const BlueprintSchema = new Schema<IBlueprint>(
     {
         name: { type: String, required: true, trim: true },
+        description: { type: String, default: "" },
         model_path: { type: String, required: true },
         data_structure: { type: Schema.Types.Mixed, required: true, default: {} },
         templates: [{ type: Schema.Types.ObjectId, ref: "template" }],
