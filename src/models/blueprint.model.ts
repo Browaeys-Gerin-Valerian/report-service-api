a55ce2cbe16ddd829dbc62f3c1e25047b48dc7cf
Valerian <valgerin@hotmail.fr>
2025-11-24 14:50:22 +0100

    delete: useless fields

--- src/models/blueprint.model.ts
+++ src/models/blueprint.model.ts
@@ -6,16 +6,9 @@ const BlueprintSchema = new Schema<IBlueprint>(
     {
         name: { type: String, required: true, trim: true },
         description: { type: String, default: "" },
-        model_path: { type: String },
         data_structure: { type: Schema.Types.Mixed, required: true, default: {} },
     },
     { timestamps: true }
 );
-
-// Pre-save hook to set model_path based on name
-BlueprintSchema.pre("save", function (next) {
-    this.model_path = `/templates/${this.name}`;
-    next();
-});
 
 export default mongoose.model<IBlueprint>("blueprints", BlueprintSchema);
