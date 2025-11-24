7ee675424c48ed08208a49e91cd02368f70bd3f0
Valerian <valgerin@hotmail.fr>
2025-11-24 14:07:17 +0100

    feat: create blueprints

--- src/router/blueprint.routes.ts
+++ src/router/blueprint.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 import { blueprintController } from "../controllers/blueprint.controller";
 const router = Router();
 
+
 router.get("/blueprints", blueprintController.getAll);
-router.post("/blueprints", blueprintController.createOne);
-router.patch("/blueprints/:id", blueprintController.updateOne);
+router.post("/blueprints", multer().none(), blueprintController.createOne);
+router.patch("/blueprints/:id", multer().none(), blueprintController.updateOne);
 router.delete("/blueprints/:id", blueprintController.deleteOne);
 
 export default router;
