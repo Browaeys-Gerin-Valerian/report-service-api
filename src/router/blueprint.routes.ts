4b570a318bf4fda6566cb084ffe615903360b1af
Valerian <valgerin@hotmail.fr>
2025-11-24 14:59:24 +0100

    feat: getOne route

--- src/router/blueprint.routes.ts
+++ src/router/blueprint.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 
 router.get("/blueprints", blueprintController.getAll);
+router.get("/blueprints/:id", blueprintController.getOneById);
 router.post("/blueprints", multer().none(), blueprintController.createOne);
 router.patch("/blueprints/:id", multer().none(), blueprintController.updateOne);
 router.delete("/blueprints/:id", blueprintController.deleteOne);
