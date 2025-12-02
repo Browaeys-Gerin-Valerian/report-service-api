6ca6370d05bf69c6e9fd39183435ff4ec1e1f2a2
Valerian <valgerin@hotmail.fr>
2025-12-02 17:30:15 +0100

    rename: from uploadTemplate to upload

--- src/router/template.routes.ts
+++ src/router/template.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { templateController } from "../controllers/template.controller";
-import { uploadTemplate } from "../middlewares/multer/uploadTemplate.middleware";
+import { upload } from "../middlewares/multer/upload.middleware";
 import { validateRequest } from "../middlewares/zod/validateRequest.middleware";
 import { createTemplateSchema, deleteTemplateSchema, getAllTemplatesSchema, getOneTemplateSchema, updateTemplateSchema } from "../middlewares/zod/schemas/template.schema";
 
@@ -9,8 +9,8 @@ const router = Router();
 
 router.get("/templates", validateRequest(getAllTemplatesSchema), templateController.getAll);
 router.get("/templates/:id", validateRequest(getOneTemplateSchema), templateController.getOneById);
-router.post("/templates", uploadTemplate.single("file"), validateRequest(createTemplateSchema), templateController.createOne);
-router.patch("/templates/:id", uploadTemplate.single("file"), validateRequest(updateTemplateSchema), templateController.updateOne);
+router.post("/templates", upload.single("file"), validateRequest(createTemplateSchema), templateController.createOne);
+router.patch("/templates/:id", upload.single("file"), validateRequest(updateTemplateSchema), templateController.updateOne);
 router.delete("/templates/:id", validateRequest(deleteTemplateSchema), templateController.deleteOne);
 
 export default router;
