26e0aea8dbec658cc172315c33f0f8d2674b6b60
Valerian <valgerin@hotmail.fr>
2025-12-05 14:17:45 +0100

    feat: setting up docx-templates, example for list and basic text and list/text generation in docx

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,21 @@
 import path from "path";
 
-export const config = {
+interface IConfig {
+    NODE_ENV: string,
+    PORT: number,
+    MONGO_URI: string,
+    DB_NAME: string | undefined,
+    ALLOWED_ORIGINS: string[],
+    TEMPLATE_DIR: string,
+    TEMPLATE_CMD_DELIMITER: string | [string, string] | undefined
+}
+
+export const config: IConfig = {
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
     MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
     DB_NAME: process.env.DB_NAME,
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["http://localhost:3000"],
     TEMPLATE_DIR: path.join(process.cwd(), "templates_files"),
+    TEMPLATE_CMD_DELIMITER: ['{', '}']
 };
--- src/services/generator/docx/docxGenerator.service.ts
+++ src/services/generator/docx/docxGenerator.service.ts
@@ -1,7 +1,22 @@
 import fs from "fs";
+import createReport from 'docx-templates';
+import { config } from "../../../config"
+import { ValidationError } from "../../../interfaces";
+const { TEMPLATE_CMD_DELIMITER } = config
 
 export async function generateDocx<T>(templatePath: string, data: T) {
-    const templateBuffer = fs.readFileSync(templatePath);
-    const buffer = {}
-    return buffer;
+    const template = fs.readFileSync(templatePath);
+    try {
+        const buffer = await createReport({
+            template,
+            data,
+            cmdDelimiter: TEMPLATE_CMD_DELIMITER,
+            errorHandler: (err, command_code) => {
+                console.log({ err, command_code })
+            },
+        });
+        return buffer;
+    } catch (error) {
+        throw new ValidationError(`Error filling DOCX: ${error}`);
+    }
 };
