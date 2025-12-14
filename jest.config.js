210668d1b5bec77857e435d27c0c9e7d2b66bc42
Valerian <valgerin@hotmail.fr>
2025-12-14 19:42:25 +0100

    fix: jest path bug

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,15 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleNameMapper: {
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@router/(.*)$': '<rootDir>/src/router/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@tests/(.*)$': '<rootDir>/src/tests/$1',
+    '^@custom_types/(.*)$': '<rootDir>/src/types/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
 };
